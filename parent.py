22de98954f9fd9cf56d84162107e2531b942e070
“HectorDevelops” <HectorDevelops@gmail.com>
2023-03-08 23:59:48 -0800

    Updated the comments

--- parent.py
+++ parent.py
@@ -8,19 +8,24 @@ class User:
     def __init__(self, name):
         self.name = name
 
+    # creating a method to greet the user 
     def say_hello(self):
         return "hello"
 
 # in the same file, add the following below the User class
 print(square(5))
+
+# instantiating a user 
 user = User("Anna")
 print(user.name)
 print(user.say_hello())
 
+# checking the variables that are avaialble in the current Namespace
+    # print(locals())
 
-
-# print(locals())
-# print(__name__)
+# checking the file created __name__ 
+    # print(__name__)
+    
 if __name__ == "__main__":
     print("the file is being executed directly")
 else:
